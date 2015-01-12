5a63a53653155e5f2b8eb674641e63e510069069
loubin <loubin@117go.com>
2015-01-12 16:29:13 +0800

    init project

---
+++ JJMyLove/JJMyLove/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JJMyLove
+//
+//  Created by iMac on 15-1-12.
+//  Copyright (c) 2015年 lb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JJMyLove/JJMyLove/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JJMyLove
+//
+//  Created by iMac on 15-1-12.
+//  Copyright (c) 2015年 lb. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
